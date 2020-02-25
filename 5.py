7abeae7bac41b603193bf10a15d454300b205b72
gRow <grovexd94@gmail.com>
2020-02-25 02:27:16 +0200

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,7 +1,6 @@
 from abc import abstractclassmethod, abstractmethod
 
-@abstractclassmethod
-class Publications(object):
+class Publication(object):
     def Publication(self, heading, text, date, autor, source, endDate):
         self.heading = heading
         self.text = text
@@ -14,7 +13,7 @@ class Publications(object):
     def printMaterial(self):
         print("Печать материала")
 
-class News(Publications):
+class News(Publication):
     def Publication(self, heading, text, date, source):
         self.heading = heading
         self.text = text
@@ -22,9 +21,9 @@ class News(Publications):
         self.source = source
 
     def printMaterial(self):
-        return "%s: %s /n Источник: %s /n дата: %s" % (self.heading, self.text, self.source, self.date)
+        return "%s : %s \n Источник: %s \n дата: %s" % (self.heading, self.text, self.source, self.date)
 
-class Advertisement(Publications):
+class Advertisement(Publication):
     def Publication(self, heading, text, date, autor):
         self.heading = heading
         self.text = text
@@ -32,22 +31,25 @@ class Advertisement(Publications):
         self.autor = autor
 
     def printMaterial(self):
-        return "%s: %s /n Автор: %s /n дата: %s" % (self.heading, self.text, self.autor, self.date)
+        return "%s: %s \n Автор: %s \n дата: %s" % (self.heading, self.text, self.autor, self.date)
 
-class Article(Publications):
+class Article(Publication):
     def Publication(self, heading, text, endDate):
         self.heading = heading
         self.text = text
         self.endDate = endDate
 
     def printMaterial(self):
-        return "%s: %s /n дата: %s" % (self.heading, self.text, self.endDate)
+        return "%s: %s \n дата: %s" % (self.heading, self.text, self.endDate)
 
 
 if __name__ == "__main__":
-    news = News(heading = "Валюта", text = "Доллар знову впав", date = "2020-02-23", source = "Нацбанк")
+    news = News()
+    news.Publication(heading = "Валюта", text = "Доллар знову впав", date = "2020-02-23", source = "Нацбанк")
     print(news.printMaterial())
-    ad = Advertisement(heading = "Евровидение", text = "Песня с которой Украина выступит на евровидении...", date = "2020-02-22", autor = "gRow09")
+    ad = Advertisement()
+    ad.Publication(heading = "Евровидение", text = "Песня с которой Украина выступит на евровидении...", date = "2020-02-22", autor = "gRow09")
     print(ad.printMaterial())
-    article = Article(heading = "Куплю квартиру", text = "звонити 067235534523", endDate = "2020-02-27")
+    article = Article()
+    article.Publication(heading = "Куплю квартиру", text = "звонити 067235534523", endDate = "2020-02-27")
     print(article.printMaterial())
